de4dd3e9540ae6d62ee977212ea2e7befd552467
Brian D <dinhlol69@gmail.com>
2020-03-26 14:19:43 -0400

    Add Apiseeds

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const DISCOGS_KEY = "TwyCggwPpiEBbWtlRqtO";
 const DISCOGS_SECRET = "dJyxJpRgpfQZEDYXTcefbxebMahziUUN";
+const APISEEDS_APIKEY = "xRcHqoqt0DBCmxCKucnYZ7WvmOHMIlkLEcfoCZnqiCFbUXoADEypBn3GITKx531b"
 
 const DISCOGS_QUERYURL = (genre, key, secret) => `https://api.discogs.com/database/search?genre=${genre}&per_page=10&page=1&key=${key}&secret=${secret}`
+const APISEEDS_QUERYURL = (artist, track, apikey) => `https://orion.apiseeds.com/api/music/lyric/${artist}/${track}?apikey=${apikey}`
 
 function fetchDiscogsData(genre) {
   // We have to append a user-agent as describe in Discogs API Documents
@@ -21,4 +23,17 @@ function fetchDiscogsData(genre) {
     .catch(error => console.error(error));
 }
 
+function fetchLyricData(artist, track) {
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  return fetch(APISEEDS_APIKEY(artist, track, APISEEDS_APIKEY), requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
 console.log(fetchDiscogsData('Hip Hop'))
+console.log(fetchLyricData('Post Malone', 'rockstar'))
