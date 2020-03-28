4aaede24c3cfd40185e5f02b84f10de0cd765583
Brian D <dinhlol69@gmail.com>
2020-03-27 23:33:54 -0400

    Add missing rows selector

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const APISEEDS_APIKEY =
 const DISCOGS_QUERYURL = (genre, key, secret) => `https://api.discogs.com/database/search?genre=${genre}&per_page=10&page=1&key=${key}&secret=${secret}`;
 const APISEEDS_QUERYURL = (artist, track, apikey) => `https://orion.apiseeds.com/api/music/lyric/${artist}/${track}?apikey=${apikey}`;
 
+const rows = document.getElementById('rows');
+
 function fetchDiscogsData(genre) {
   // We have to append a user-agent as describe in Discogs API Documents
   // https://www.discogs.com/developers#page:home,header:home-general-information
